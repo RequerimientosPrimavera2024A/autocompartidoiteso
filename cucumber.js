9a2ad0daddf8d19057c34e4879ff74261c17db9f
axelgallardo100900 <axelgallardo100900@gmail.com>
2024-05-05 22:36:40 -0600

    Solo lo escencial
    Eliminados archivos sobrantes

--- cucumber.js
+++
@@ -1,8 +0,0 @@
-<<<<<<< HEAD
-module.exports = {
-    default: `--format-options '{"snippetInterface": "synchronous"}'`
-=======
-module.exports = {
-    default: `--format-options '{"snippetInterface": "synchronous"}'`
->>>>>>> 92a7cb87c22e688a2945a65cc1c82be762028d4f
-  }
